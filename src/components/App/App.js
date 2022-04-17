11ba3a02541dbbb6f3068c5dc2dff5f0b93e7fe4
abdullahbr98 <abdullahbr98@gmail.com>
2022-04-18 02:56:18 +0500

    Initial Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { SignUp } from '../SignUp/SignUp';
+import { LogIn } from '../LogIn/login';
+
+
+function App() {
+  return (
+     <div>
+       {/* <SignUp /> */}
+       <LogIn />
+     </div>
+  );
+}
+
+export default App;
